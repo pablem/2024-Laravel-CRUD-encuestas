a3f55e524d9dcc2fb7bc9b9135c102e43063bb62
pablem <palmierijpablo@gmail.com>
2024-02-28 09:20:20 -0300

    Revert "Esto es una prueba desde react"
    This reverts commit ebdb6d3fad531350506edf7bfbe51399ad508518.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
-import './Prueba';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
+
 Alpine.start();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div id="estoEsUnaPrueba"></div>
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
